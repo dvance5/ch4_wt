ccc6c4f429348a9780a9e8f7b5a92bebdf22c87b
Donald Vance <dvance5@uncc.edu>
2017-10-12 12:07:41 +0000

    Done 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+  
+  # in movies_controller.rb
+  def create
+    params.require(:movie)
+    permitted = params[:movie].permit(:title,:rating,:release_date)
+    @movie = Movie.create!(permitted)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+# in movies_controller.rb
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    params.require(:movie)
+    permitted = params[:movie].permit(:title,:rating,:release_date)
+    @movie.update_attributes!(permitted)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+end
